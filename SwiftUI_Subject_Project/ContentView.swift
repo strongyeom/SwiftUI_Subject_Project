b4dd0a8bd7c8dee2f6f2685df81de675e71bbb7c
yeomsungpil <present7256@gmail.com>
2023-11-16 01:30:19 +0900

    Initial Commit

---
+++ SwiftUI_Subject_Project/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_Subject_Project
+//
+//  Created by 염성필 on 2023/11/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Subject_Project/SwiftUI_Subject_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Subject_ProjectApp.swift
+//  SwiftUI_Subject_Project
+//
+//  Created by 염성필 on 2023/11/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Subject_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
